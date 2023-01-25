7ca542148e23b8c84f11aede6f31216d23187380
Louis <louis.kirkpatrick@landregistry.gov.uk>
2023-01-25 13:50:24 +0000

    added search support for the repo

--- create_output_file.py
+++ create_output_file.py
@@ -4,11 +4,19 @@ base_url = 'https://landregistry.github.io/llc-release-notices/releases/'
 
 output = []
 
-for file_object in os.scandir('releases'):
-  filename = file_object.name
-  if filename.endswith('.pdf'):
-    link = f'{base_url}{filename}'.replace(' ', '%20')
-    output.append({"name": filename.replace('.pdf', ''), "link": link})
+files = {'maintain': 'index.json', 'search': 'search.json'}
 
-with open('index.json', 'w') as outputfile:
-  json.dump(output, outputfile)
+def create_output(service):
+  output = []
+
+  for file_object in os.scandir(f'releases/{service}'):
+    filename = file_object.name
+    if filename.endswith('.pdf'):
+      link = f'{base_url}{service}/{filename}'.replace(' ', '%20')
+      output.append({"name": filename.replace('.pdf', ''), "link": link})
+
+    with open(files[service], 'w') as outputfile:
+      json.dump(output, outputfile)
+
+create_output('maintain')
+create_output('search')
